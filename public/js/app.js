1c6fb9bfdf37a96366c8978c3b02a217cd4f1b5d
Sergey <bondsergey2012@gmail.com>
2014-08-27 16:27:54 +0400

    base commit

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.router', 'mean.system']);
+
+angular.module('mean.system', ['ngResource']);
---
+++ public/js/config.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('mean').config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'views/index.html'
+            }).otherwise({
+                redirectTo: '/'
+            });
+    }
+]);
+
+//Setting HTML5 Location Mode
+//angular.module('mean').config(['$locationProvider',
+//  function($locationProvider) {
+//    $locationProvider.hashPrefix('!');
+//}
+//]);
